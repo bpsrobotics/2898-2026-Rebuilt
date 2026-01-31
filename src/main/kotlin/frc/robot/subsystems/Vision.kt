85f801af4a3d0a17ac6f3d09a7cf31ee53ad421d
BubbleShade <ozypfl@gmail.com>
2026-01-31 12:29:21 -0800

    Gerbabble

--- src/main/kotlin/frc/robot/subsystems/Vision.kt
+++ src/main/kotlin/frc/robot/subsystems/Vision.kt
@@ -5,6 +5,7 @@ import beaverlib.utils.Units.Angular.degrees
 import beaverlib.utils.Units.Linear.inches
 import edu.wpi.first.apriltag.AprilTag
 import edu.wpi.first.apriltag.AprilTagFieldLayout
+import edu.wpi.first.apriltag.AprilTagFields
 import edu.wpi.first.math.geometry.Pose3d
 import edu.wpi.first.math.geometry.Rotation3d
 import edu.wpi.first.math.geometry.Transform3d
@@ -19,7 +20,8 @@ val customField: MutableList<AprilTag> = mutableListOf(AprilTag(1, Pose3d()))
 // val customVision = AprilTagFieldLayout.(mutableListOf<AprilTag>(
 //    AprilTag(4, Pose3d(0.0, 0.0, 0.0, Rotation3d()
 //    ))))
-
+val bunnyBotsLayout =
+    AprilTagFieldLayout(File(Filesystem.getDeployDirectory(), "2025-bunnybots.json").path)
 val Vision =
     BeaverPhotonVision(
         BeaverVisionCamera(
@@ -31,10 +33,7 @@ val Vision =
                 Rotation3d(0.0, 30.0.degrees.asRadians, PI + 7.0.degrees.asRadians),
             ),
             //            Transform3d(0.18, -0.33, 0.2, Rotation3d()),
-            layout =
-                AprilTagFieldLayout(
-                    File(Filesystem.getDeployDirectory(), "2025-bunnybots.json").path
-                ),
+            layout = AprilTagFieldLayout.loadField(AprilTagFields.k2026RebuiltWelded),
             strategy = PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
             fallbackStrategy = PhotonPoseEstimator.PoseStrategy.CLOSEST_TO_REFERENCE_POSE,
         )

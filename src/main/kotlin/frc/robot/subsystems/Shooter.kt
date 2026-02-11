74c28ed5c46e94ae9e9d1d6e526aa4024083e597
BubbleShade <ozypfl@gmail.com>
2026-02-10 17:42:42 -0800

    Simplified Code

--- src/main/kotlin/frc/robot/subsystems/Shooter.kt
+++ src/main/kotlin/frc/robot/subsystems/Shooter.kt
@@ -1,28 +1,16 @@
 package frc.robot.subsystems
 
-import beaverlib.utils.Units.Angular.radiansPerSecond
+import beaverlib.controls.PIDConstants
+import beaverlib.controls.PIDFF
+import beaverlib.controls.SimpleMotorFeedForwardConstants
 import com.revrobotics.PersistMode
 import com.revrobotics.ResetMode
 import com.revrobotics.spark.SparkLowLevel
 import com.revrobotics.spark.SparkMax
 import com.revrobotics.spark.config.SparkBaseConfig
 import com.revrobotics.spark.config.SparkMaxConfig
-import edu.wpi.first.math.controller.PIDController
-import edu.wpi.first.math.controller.SimpleMotorFeedforward
-import edu.wpi.first.math.trajectory.TrapezoidProfile
-import edu.wpi.first.wpilibj.Encoder
 import edu.wpi.first.wpilibj2.command.SubsystemBase
-import frc.robot.subsystems.Shooter.Constants.botkD
-import frc.robot.subsystems.Shooter.Constants.botkI
-import frc.robot.subsystems.Shooter.Constants.botkP
-import frc.robot.subsystems.Shooter.Constants.bottomencoder
-import frc.robot.subsystems.Shooter.Constants.maxAngularAcceleration
-import frc.robot.subsystems.Shooter.Constants.maxAngularvelocity
 import frc.robot.subsystems.Shooter.Constants.tempIDbottom
-import frc.robot.subsystems.Shooter.Constants.topencoder
-import frc.robot.subsystems.Shooter.Constants.topkD
-import frc.robot.subsystems.Shooter.Constants.topkI
-import frc.robot.subsystems.Shooter.Constants.topkP
 import frc.robot.subsystems.Shooter.Constants.toptempID
 
 object Shooter : SubsystemBase() {
@@ -33,12 +21,13 @@ object Shooter : SubsystemBase() {
         val tempIDbottom = 1
         val topencoder = 99
         val bottomencoder = 98
-        val topkP = 0.0
-        val topkI = 0.0
-        val topkD = 0.0
-        val botkP = 0.0
-        val botkI = 0.0
-        val botkD = 0.0
+
+        val topPIDConstants = PIDConstants(0.0, 0.0, 0.0)
+        val bottomPIDConstants = PIDConstants(0.0, 0.0, 0.0)
+
+        val topFFConstants = SimpleMotorFeedForwardConstants(0.1, 0.19, 4.04)
+        val bottomFFConstants = SimpleMotorFeedForwardConstants(0.1, 0.19, 4.04)
+
         // todo ensure all values for velocity are in rads/sec
     }
 
@@ -46,35 +35,15 @@ object Shooter : SubsystemBase() {
     val bottomMotor = SparkMax(tempIDbottom, SparkLowLevel.MotorType.kBrushless)
 
     private var ShooterConfig: SparkMaxConfig = SparkMaxConfig()
-    private val Encoder = Encoder(topencoder, bottomencoder)
-    private val constraints =
-        TrapezoidProfile.Constraints(maxAngularvelocity, maxAngularAcceleration)
-    private var profile = TrapezoidProfile(constraints)
-    var currentState =
-        TrapezoidProfile.State(
-            Encoder.distance,
-            0.0,
-        ) // todo configure this to take actual motor speeds
-    var goalState = TrapezoidProfile.State(Encoder.distance, 0.0)
-    val TopPIDController = PIDController(topkP, topkI, topkD)
-    val TopFF: SimpleMotorFeedforward = SimpleMotorFeedforward(0.0, 0.0, 0.0)
-    val BotPIDController = PIDController(botkP, botkI, botkD)
-    val BotFF: SimpleMotorFeedforward = SimpleMotorFeedforward(0.0, 0.0, 0.0)
 
-    // Shooter speed:
-    val topMotorSpeed
-        // you can also define motor speed using angular setpoints
-        get() = topencoder.radiansPerSecond
-
-    val bottomMotorSpeed
-        // you can also define motor speed using angular setpoints
-        get() = bottomencoder.radiansPerSecond
+    val TopPIDFF = PIDFF(Constants.topPIDConstants, Constants.topFFConstants)
+    val BottomPIDFF = PIDFF(Constants.bottomPIDConstants, Constants.bottomFFConstants)
 
     init {
         ShooterConfig.idleMode(SparkBaseConfig.IdleMode.kCoast)
-            .smartCurrentLimit(40)
+            .smartCurrentLimit(20)
             .encoder
-            .velocityConversionFactor(1.0)
+            .velocityConversionFactor(2.0)
         topMotor.configure(
             ShooterConfig,
             ResetMode.kResetSafeParameters,
@@ -87,14 +56,5 @@ object Shooter : SubsystemBase() {
         )
     }
 
-    fun calculateVoltage() {
-        topMotor.set(
-            TopPIDController.calculate(currentState.velocity, goalState.velocity) +
-                TopFF.calculate(goalState.velocity)
-        )
-        bottomMotor.set(
-            BotPIDController.calculate(currentState.velocity, goalState.velocity) +
-                BotFF.calculate(goalState.velocity)
-        )
-    }
+    fun doMaintainSpeed() = this.run {}
 }

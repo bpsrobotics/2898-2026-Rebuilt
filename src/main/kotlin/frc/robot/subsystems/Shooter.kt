995e5e7549ef9ded6300ea6baeeae89dbefbe3a3
Ant_Throw_Pology <antthrowpology@gmail.com>
2026-02-11 16:31:40 -0800

    Finish out flywheel code

--- src/main/kotlin/frc/robot/subsystems/Shooter.kt
+++ src/main/kotlin/frc/robot/subsystems/Shooter.kt
@@ -9,52 +9,72 @@ import com.revrobotics.spark.SparkLowLevel
 import com.revrobotics.spark.SparkMax
 import com.revrobotics.spark.config.SparkBaseConfig
 import com.revrobotics.spark.config.SparkMaxConfig
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.Commands.waitUntil
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup
 import edu.wpi.first.wpilibj2.command.SubsystemBase
-import frc.robot.subsystems.Shooter.Constants.tempIDbottom
-import frc.robot.subsystems.Shooter.Constants.toptempID
 
 object Shooter : SubsystemBase() {
     object Constants {
-        val maxAngularvelocity = 60.0 // radians/second
-        val maxAngularAcceleration = 10.0 // radians/second^2
-        val toptempID = 0
-        val tempIDbottom = 1
-        val topencoder = 99
-        val bottomencoder = 98
+        // TODO(ant): Get CAN IDs from Electrical
+        const val MOTOR_1_ID = 70
+        const val MOTOR_2_ID = 71
 
-        val topPIDConstants = PIDConstants(0.0, 0.0, 0.0)
-        val bottomPIDConstants = PIDConstants(0.0, 0.0, 0.0)
+        val motor1PIDConstants = PIDConstants(0.0, 0.0, 0.0)
+        val motor2PIDConstants = PIDConstants(0.0, 0.0, 0.0)
 
-        val topFFConstants = SimpleMotorFeedForwardConstants(0.1, 0.19, 4.04)
-        val bottomFFConstants = SimpleMotorFeedForwardConstants(0.1, 0.19, 4.04)
-
-        // todo ensure all values for velocity are in rads/sec
+        val motor1FFConstants = SimpleMotorFeedForwardConstants(0.1, 0.19, 4.04)
+        val motor2FFConstants = SimpleMotorFeedForwardConstants(0.1, 0.19, 4.04)
     }
 
-    val topMotor = SparkMax(toptempID, SparkLowLevel.MotorType.kBrushless)
-    val bottomMotor = SparkMax(tempIDbottom, SparkLowLevel.MotorType.kBrushless)
+    val motor1 = SparkMax(Constants.MOTOR_1_ID, SparkLowLevel.MotorType.kBrushless)
+    val motor2 = SparkMax(Constants.MOTOR_2_ID, SparkLowLevel.MotorType.kBrushless)
 
-    private var ShooterConfig: SparkMaxConfig = SparkMaxConfig()
-
-    val TopPIDFF = PIDFF(Constants.topPIDConstants, Constants.topFFConstants)
-    val BottomPIDFF = PIDFF(Constants.bottomPIDConstants, Constants.bottomFFConstants)
+    val motor1PIDFF = PIDFF(Constants.motor1PIDConstants, Constants.motor1FFConstants)
+    val motor2PIDFF = PIDFF(Constants.motor2PIDConstants, Constants.motor2FFConstants)
 
     init {
-        ShooterConfig.idleMode(SparkBaseConfig.IdleMode.kCoast)
+        val shooterConfig = SparkMaxConfig()
+        shooterConfig
+            .idleMode(SparkBaseConfig.IdleMode.kCoast)
             .smartCurrentLimit(20)
             .encoder
             .velocityConversionFactor(2.0)
-        topMotor.configure(
-            ShooterConfig,
+        motor1.configure(
+            shooterConfig,
             ResetMode.kResetSafeParameters,
             PersistMode.kPersistParameters,
         )
-        bottomMotor.configure(
-            ShooterConfig.inverted(true),
+        motor2.configure(
+            shooterConfig.inverted(true),
             ResetMode.kResetSafeParameters,
             PersistMode.kPersistParameters,
         )
+
+        defaultCommand = stop()
     }
 
-    fun doMaintainSpeed() = this.run {}
+    fun stop(): Command {
+        return runOnce {
+            motor1.stopMotor()
+            motor2.stopMotor()
+        }
+    }
+
+    fun runSpeed(setpointAngVelocity: Double): Command {
+        return run {
+            motor1PIDFF.setpoint = setpointAngVelocity
+            motor1.set(motor1PIDFF.calculate(motor1.encoder.velocity))
+
+            motor2PIDFF.setpoint = setpointAngVelocity
+            motor2.set(motor2PIDFF.calculate(motor2.encoder.velocity))
+        }
+    }
+
+    fun waitSpeed(setpointAngVelocity: Double): Command {
+        return ParallelRaceGroup(
+            runSpeed(setpointAngVelocity),
+            waitUntil({ motor1PIDFF.atSetpoint() && motor2PIDFF.atSetpoint() }),
+        )
+    }
 }

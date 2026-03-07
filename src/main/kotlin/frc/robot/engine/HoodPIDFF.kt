234480c19fa5be33c68f3da33dbeb637d573f15d
BubbleShade <ozypfl@gmail.com>
2026-03-06 17:03:02 -0800

    Hood PIDFF no longer uses WPI feed forward

--- src/main/kotlin/frc/robot/engine/HoodPIDFF.kt
+++ src/main/kotlin/frc/robot/engine/HoodPIDFF.kt
@@ -4,22 +4,32 @@ import beaverlib.controls.ArmFeedForwardConstants
 import beaverlib.controls.PIDConstants
 import beaverlib.utils.Units.Angular.AngleUnit
 import beaverlib.utils.Units.Angular.AngularVelocity
-import beaverlib.utils.Units.Angular.degrees
 import beaverlib.utils.Units.Angular.radians
 import beaverlib.utils.Units.Angular.radiansPerSecond
-import edu.wpi.first.math.controller.ArmFeedforward
 import edu.wpi.first.math.controller.PIDController
 import edu.wpi.first.util.sendable.Sendable
 import edu.wpi.first.util.sendable.SendableBuilder
-import kotlin.math.absoluteValue
+import kotlin.math.sign
 
-/** A combined Proportional Integral Derivative and an Arm Feed Forward controller. */
-open class HoodPIDFF(pidConstants: PIDConstants, ffConstants: ArmFeedForwardConstants) : Sendable {
+/**
+ * A combined Proportional Integral Derivative and an Arm Feed Forward controller.
+ *
+ * @param pidConstants The P I and D values to use for this controller
+ * @param ffConstants The arm feedforward constants to use for this controller
+ * @param zeroPosition the angle at which the endEffector is level with the ground (Where gravity
+ *   will affect it the most)
+ */
+open class HoodPIDFF(
+    pidConstants: PIDConstants,
+    ffConstants: ArmFeedForwardConstants,
+    var zeroPosition: AngleUnit = 0.0.radians,
+) : Sendable {
     /** The Proportional Integral Derivative controller part of the PidFF */
     val pid = PIDController(pidConstants.p, pidConstants.i, pidConstants.d)
-    /** The FeedForward part of the PidFF */
-    val feedforward = ArmFeedforward(ffConstants.kS, 0.0, ffConstants.kV, ffConstants.kA)
+    var kS = ffConstants.kS
     var kG = ffConstants.kG
+    var kV = ffConstants.kV
+    var kA = ffConstants.kA
 
     /** The goal state for the PidFF */
     var setpoint: AngleUnit
@@ -36,11 +46,16 @@ open class HoodPIDFF(pidConstants: PIDConstants, ffConstants: ArmFeedForwardCons
      */
     open fun calculate(
         measurement: AngleUnit,
-        velocity: AngularVelocity = 0.radiansPerSecond,
+        desiredVelocity: AngularVelocity = 0.radiansPerSecond,
     ): Double {
-        return pid.calculate(measurement.asRadians) +
-            feedforward.calculate(measurement.asRadians, velocity.asRadiansPerSecond) +
-            (kG * (setpoint - 22.degrees).sin().absoluteValue)
+        var voltage = pid.calculate(measurement.asRadians)
+        if (desiredVelocity != 0.radiansPerSecond) {
+            voltage += sign(desiredVelocity.asRadiansPerSecond) * kS
+        } else if (!pid.atSetpoint()) voltage += sign(voltage) * kS
+        voltage += (setpoint - zeroPosition).cos() * kG
+        return voltage +
+            (kV * desiredVelocity.asRadiansPerSecond) +
+            (kA * desiredVelocity.asRadiansPerSecond)
     }
 
     /** Returns this PID [PIDController.atSetpoint] */
@@ -49,10 +64,10 @@ open class HoodPIDFF(pidConstants: PIDConstants, ffConstants: ArmFeedForwardCons
     }
 
     override fun initSendable(builder: SendableBuilder) {
-        builder.addDoubleProperty("kS", feedforward::getKs, feedforward::setKs)
-        builder.addDoubleProperty("kG", { kG }, { v: Double -> kG = v })
-        builder.addDoubleProperty("kV", feedforward::getKv, feedforward::setKv)
-        builder.addDoubleProperty("kA", feedforward::getKa, feedforward::setKa)
+        builder.addDoubleProperty("kS", { kS }, { kS = it })
+        builder.addDoubleProperty("kG", { kG }, { kG = it })
+        builder.addDoubleProperty("kV", { kV }, { kV = it })
+        builder.addDoubleProperty("kA", { kA }, { kA = it })
         builder.addDoubleProperty("kP", pid::getP, pid::setP)
         builder.addDoubleProperty("kI", pid::getI, pid::setI)
         builder.addDoubleProperty("kD", pid::getD, pid::setD)
